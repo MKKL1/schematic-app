7f60f1306eaa20c1990f6d780d81eefded538a64
MKKL <Mkkl3301@gmail.com>
2025-01-26 16:20:31 +0100

    Add cache to post repo

--- server/internal/services/post-service/cmd/application.go
+++ server/internal/services/post-service/cmd/application.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"context"
+	"github.com/MKKL1/schematic-app/server/internal/pkg/rueidisaside"
 	"github.com/MKKL1/schematic-app/server/internal/pkg/server"
 	"github.com/MKKL1/schematic-app/server/internal/services/post-service/app"
 	"github.com/MKKL1/schematic-app/server/internal/services/post-service/app/query"
 	"github.com/MKKL1/schematic-app/server/internal/services/post-service/postgres"
 	"github.com/MKKL1/schematic-app/server/internal/services/post-service/postgres/db"
+	"github.com/MKKL1/schematic-app/server/internal/services/post-service/redis"
+	"github.com/redis/rueidis"
+	"time"
 )
 
 func NewApplication(ctx context.Context) app.Application {
@@ -24,17 +28,17 @@ func NewApplication(ctx context.Context) app.Application {
 	queries := db.New(dbPool)
 	postRepo := postgres.NewPostPostgresRepository(queries)
 
-	//clientRed := server.NewRedisClient()
-	////TODO Move somewhere else
-	//client, err := rueidisaside.NewClient(rueidisaside.ClientOption{
-	//	ClientBuilder: func(option rueidis.ClientOption) (rueidis.Client, error) {
-	//		return clientRed, nil
-	//	},
-	//	ClientOption: rueidis.ClientOption{},
-	//	ClientTTL:    time.Minute,
-	//})
+	clientRed := server.NewRedisClient()
+	//TODO Move somewhere else
+	client, err := rueidisaside.NewClient(rueidisaside.ClientOption{
+		ClientBuilder: func(option rueidis.ClientOption) (rueidis.Client, error) {
+			return clientRed, nil
+		},
+		ClientOption: rueidis.ClientOption{},
+		ClientTTL:    time.Minute,
+	})
 
-	//userCacheRepo := redis.NewCacheRepository(userRepo, client)
+	postCacheRepo := redis.NewPostCacheRepository(postRepo, client)
 
 	//idNode, err := snowflake.NewNode(1)
 	//if err != nil {
@@ -44,7 +48,7 @@ func NewApplication(ctx context.Context) app.Application {
 	return app.Application{
 		Commands: app.Commands{},
 		Queries: app.Queries{
-			GetPostById: query.NewGetPostByIdHandler(postRepo),
+			GetPostById: query.NewGetPostByIdHandler(postCacheRepo),
 		},
 	}
 }
---
+++ server/internal/services/post-service/redis/post_cache_repository.go
@@ -0,0 +1,70 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"github.com/MKKL1/schematic-app/server/internal/pkg/rueidisaside"
+	"github.com/MKKL1/schematic-app/server/internal/services/post-service/domain/post"
+	postProto "github.com/MKKL1/schematic-app/server/internal/services/post-service/domain/post/proto"
+	"github.com/golang/protobuf/proto"
+	"time"
+)
+
+type PostCacheRepository struct {
+	baseRepo    post.Repository
+	typedClient rueidisaside.TypedCacheAsideClient[post.Model]
+}
+
+func NewPostCacheRepository(baseRepo post.Repository, cacheClient rueidisaside.CacheAsideClient) *PostCacheRepository {
+	typedClient := rueidisaside.NewTypedCacheAsideClient(
+		cacheClient,
+		func(dbPost *post.Model) (string, error) {
+			pbModel := postProto.PostModel{
+				Id:    dbPost.ID,
+				Desc:  dbPost.Description,
+				Owner: dbPost.Owner,
+				AName: dbPost.AuthorName,
+				AId:   dbPost.AuthorID,
+			}
+			marshal, err := proto.Marshal(&pbModel)
+			if err != nil {
+				return "", err
+			}
+			return string(marshal), err
+		},
+		func(d string) (*post.Model, error) {
+			pbModel := postProto.PostModel{}
+			err := proto.Unmarshal([]byte(d), &pbModel)
+			if err != nil {
+				return nil, err
+			}
+
+			postModel := &post.Model{
+				ID:          pbModel.Id,
+				Description: pbModel.Desc,
+				Owner:       pbModel.Owner,
+				AuthorName:  pbModel.AName,
+				AuthorID:    pbModel.AId,
+			}
+
+			return postModel, nil
+		},
+	)
+
+	return &PostCacheRepository{baseRepo: baseRepo, typedClient: typedClient}
+}
+
+func (p PostCacheRepository) FindById(ctx context.Context, id int64) (post.Model, error) {
+	val, err := p.typedClient.Get(ctx, 10*time.Minute, fmt.Sprint("post:", id), func(ctx context.Context, key string) (val *post.Model, err error) {
+		postModel, err := p.baseRepo.FindById(ctx, id)
+		if err != nil {
+			return nil, err
+		}
+
+		return &postModel, nil
+	})
+	if val == nil {
+		return post.Model{}, fmt.Errorf("post not found")
+	}
+	return *val, err
+}
